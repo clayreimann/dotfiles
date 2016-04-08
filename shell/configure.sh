69c4555a9e49e05027800d2f3f0b76c30e8e004d
Clay Reimann <clayreimann@yahoo-inc.com>
2016-04-08 09:57:19 -0500

    Initial commit

---
+++ shell/configure.sh
@@ -0,0 +1,9 @@
+#
+# Runs during a new installation to bootstrap bash-it
+#
+
+source ~/.bash_profile
+
+bash-it enable alias general
+bash-it enable completion bash-it system git gradle gulp
+bash-it enable plugin autojump
